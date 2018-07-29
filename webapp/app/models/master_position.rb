7bc8405f07bbfb5072d35846e94d0d676abfcf4c
Choi-CY <ths0023@naver.com>
2018-07-30 03:04:13 +0900

    re_date_ui

--- webapp/app/models/master_position.rb
+++ webapp/app/models/master_position.rb
@@ -1,8 +1,8 @@
 class MasterPosition < ApplicationRecord
-<<<<<<< HEAD
+
   has_many :projects
   has_many :user_infos, through: :projects
-=======
+
   has_many :user_infos
->>>>>>> 392dabb662ee489a8b8385e71068dee14dc82bcd
+
 end
--- webapp/app/models/user_info.rb
+++ webapp/app/models/user_info.rb
@@ -1,8 +1,8 @@
 class UserInfo < ApplicationRecord
-<<<<<<< HEAD
+
   has_many :projects
   has_many :master_position, through: :projects
-=======
+
   belongs_to :master_position
->>>>>>> 392dabb662ee489a8b8385e71068dee14dc82bcd
+
 end
